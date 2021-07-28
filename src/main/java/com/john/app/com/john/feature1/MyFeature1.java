6c605915faf9fd4a4d73efd39b0bc21b25b68a42
richmejc2010 <richard_mejia@hotmail.com>
2021-07-27 19:28:23 -0500

    estos son los cambios de John donde creo una carpeta y una clase

---
+++ src/main/java/com/john/app/com/john/feature1/MyFeature1.java
@@ -0,0 +1,22 @@
+package com.john.app.com.john.feature1;
+
+public class MyFeature1 {
+    public String name = "john";
+    public String lastName = "didi";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
