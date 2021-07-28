c78cf63e058ceaa46bd6c515715a513cc3dfe05d
richmejc2010 <richard_mejia@hotmail.com>
2021-07-27 19:22:48 -0500

    usuario richard creo clase MyFeature2

---
+++ src/main/java/com/john/app/com/john/feature2/MyFeature2.java
@@ -0,0 +1,14 @@
+package com.john.app.com.john.feature2;
+
+public class MyFeature2 {
+
+    private String variable = "Este codigo fue hecho por la persona que esta trabajando en la rama-feature2";
+    
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
+}
